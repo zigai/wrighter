2597a0578ae187e456ec15e12680433123eb8d48
ziga-ivansek <ziga.ivansek@protonmail.com>
2022-04-30 13:22:01 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,27 @@
-from setuptools import setup, find_packages
-with open('./requirements.txt') as f:
-    requirements = f.read().splitlines()
+import pathlib
+
+from setuptools import find_packages, setup
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+REQUIREMENTS = (HERE / "requirements.txt").read_text()
 
 setup(name="wrighter",
       version="0.0.2",
-      description="Quickly build web scrapers with Playwright. ",
+      description="Quickly build web scrapers with Playwright",
+      long_description=README,
+      long_description_content_type="text/markdown",
       author="Ziga Ivansek",
+      author_email="ziga.ivansek@gmail.com",
+      url="https://github.com/zigai/wrighter",
+      license="MIT",
+      classifiers=[
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 3.8",
+          "Programming Language :: Python :: 3.9",
+          "Programming Language :: Python :: 3.10",
+          "Programming Language :: Python :: 3 :: Only",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
       packages=find_packages(),
-      install_requires=requirements)
+      install_requires=REQUIREMENTS)
