23ff5570b9709a3f71a859bef62dcda22160efd4
ziga-ivansek <ziga.ivansek@protonmail.com>
2022-05-22 10:35:03 +0200

    add screenshots, fingerprint testing

--- wrighter/wrighter.py
+++ wrighter/wrighter.py
@@ -12,7 +12,7 @@ from playwright.async_api import async_playwright
 from playwright.sync_api import (Browser, BrowserContext, Page, Playwright, Response,
                                  sync_playwright)
 from playwright_stealth import stealth_sync
-from stdl import fs
+from stdl import fs, str_util
 from stdl.datetime_util import DateTime
 
 from wrighter.playwright_settings import PlaywrightSettings
@@ -32,8 +32,10 @@ class Wrighter:
         self.headless = headless
         self.settings = settings
         self.stealth = stealth
-        self.data_dir = data_dir
-        fs.assert_paths_exist(self.data_dir)
+        self.data_dir = os.path.abspath(data_dir)
+        if not os.path.exists(self.data_dir):
+            LOG.warning(f"'{self.data_dir}' doesn't exist, creating it.")
+            os.makedirs(self.data_dir)
         self.browser_driver = browser
         self.browser: Browser = self.__get_browser(browser=self.browser_driver)
         self.context: BrowserContext = self.__get_context(settings=self.settings)
@@ -93,7 +95,8 @@ class Wrighter:
         self.page.wait_for_load_state()
         return response
 
-    def find_urls(self, html: str):
+    @staticmethod
+    def find_urls(html: str):
         """Find all urls inside HTML"""
         URL_PATTERN = re.compile(r"^(https:\/\/|www\..+\..+)")
         soup = BeautifulSoup(html, "html.parser")
@@ -120,26 +123,42 @@ class Wrighter:
                 "Pass the save path to this method call or define 'storage_state' in PlaywrightSettings."
             )
 
-    def __get_default_data_save_path(self):
-        datetime = DateTime.from_timestamp_as_str(time.time(), date_sep="-", time_sep="-")
-        datetime = datetime.replace(", ", ".")
-        return f"{self.data_dir}{os.sep}{datetime}_data.json"
+    def __get_default_filename(self):
+        datetime = DateTime.from_timestamp_as_str(time.time(), date_sep="-",
+                                                  time_sep="-").replace(", ", ".")
+        return f"{datetime}_data.json"
 
-    def dump_data_to_json(self, path: str | None = None):
-        """Save the scraped data to a json file."""
-        if len(self.data) == 0 or self.data is None:
+    def dump_data_to_json(self, data=None, filename: str | None = None):
+        """Save the scraped data to a json file. By default saves data that is in 'self.data'"""
+        if (len(self.data) == 0 or self.data is None) and data is None:
             LOG.warning("No data to save.")
             return False
 
-        if path is not None:
-            fs.assert_paths_exist(os.path.dirname(path))
-            save_path = path
+        if filename is not None:
+            save_path = self.data_dir + os.sep + filename
+            if os.path.exists(save_path):
+                save_path = self.data_dir + os.sep + self.__get_default_filename()
+                LOG.warning(f"Specified filename already exits.")
+                LOG.info(f"Saving data to default save path ({save_path})")
         else:
-            save_path = self.__get_default_data_save_path()
+            save_path = self.data_dir + os.sep + self.__get_default_filename()
             LOG.info(f"Saving data to default save path ({save_path})")
+        if data is None:
+            fs.json_dump(save_path, self.data)
+        else:
+            fs.json_dump(save_path, data)
 
-        fs.json_dump(save_path, self.data)
         return True
+
+    def screenshot(self, filename: str = None, full_page: bool = True):
+        directory = f"{self.data_dir}{os.sep}screenshots"
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        if filename is None:
+            filename = f"screenshot.{self.page.title()}.{str_util.FilterStr.file_name(self.page.url)}.png"
+        path = f"{directory}{os.sep}{filename}"
+        LOG.info(f"Saving a screenshot of {self.page.url} to '{path}'")
+        self.page.screenshot(type="png", path=path, full_page=full_page)
 
     def print_config(self):
         """Print the configuration of Wrighter instance"""
@@ -149,6 +168,22 @@ class Wrighter:
         print(f"Data directory: {self.data_dir}")
         if self.settings is not None:
             self.settings.print()
+
+    def test_browser_fingerprint(self):
+        datetime = DateTime.from_timestamp_as_str(time.time(), date_sep="-",
+                                                  time_sep="-").replace(", ", ".")
+        self.load_page("https://bot.sannysoft.com/")
+        self.sleep_for(seconds=3)
+        self.screenshot(filename=f"{datetime}_test_fingerprint.png")
+
+        self.load_page("https://antcpt.com/eng/information/demo-form/recaptcha-3-test-score.html")
+        self.sleep_for(seconds=6)
+        self.screenshot(filename=f"{datetime}_test_recaptcha3.png")
+
+        self.load_page("https://pixelscan.net/")
+        self.sleep_for(seconds=18)
+
+        self.screenshot(filename=f"{datetime}_test_pixelscan.png")
 
     def run(self):
         """Write your scraper logic here."""
