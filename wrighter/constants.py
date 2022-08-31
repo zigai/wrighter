62eedbe42eb8b1fc8abd5b74c7f30a5eda151ff0
zigai <ziga.ivansek@gmail.com>
2022-08-31 19:19:04 +0200

    refactor: constants

---
+++ wrighter/constants.py
@@ -0,0 +1,10 @@
+PERMISSIONS = [
+    'geolocation', 'midi', 'midi-sysex', 'notifications', 'camera', 'microphone', 'background-sync',
+    'ambient-light-sensor', 'accelerometer', 'gyroscope', 'magnetometer', 'accessibility-events',
+    'clipboard-read', 'clipboard-write', 'payment-handler'
+]
+BROWSERS = ["firefox", "chromium", "webkit"]
+CHANNELS = [
+    "chrome", "chrome-beta", "chrome-dev", "chrome-canary", "msedge", "msedge-beta", "msedge-dev",
+    "msedge-canary"
+]
