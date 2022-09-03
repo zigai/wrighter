67dff117518709518f8f8913d3007f9bd65dd461
zigai <ziga.ivansek@gmail.com>
2022-09-03 23:04:58 +0200

    feat: resource types

--- wrighter/constants.py
+++ wrighter/constants.py
@@ -1,11 +1,16 @@
 PERMISSIONS = [
-    'geolocation', 'midi', 'midi-sysex', 'notifications', 'camera',
-    'microphone', 'background-sync', 'ambient-light-sensor', 'accelerometer',
-    'gyroscope', 'magnetometer', 'accessibility-events', 'clipboard-read',
-    'clipboard-write', 'payment-handler'
+    "geolocation", "midi", "midi-sysex", "notifications", "camera",
+    "microphone", "background-sync", "ambient-light-sensor", "accelerometer",
+    "gyroscope", "magnetometer", "accessibility-events", "clipboard-read",
+    "clipboard-write", "payment-handler"
 ]
 BROWSERS = ["firefox", "chromium", "webkit"]
 CHANNELS = [
     "chrome", "chrome-beta", "chrome-dev", "chrome-canary", "msedge",
     "msedge-beta", "msedge-dev", "msedge-canary"
 ]
+RESOURCE_TYPES = [
+    "document", "stylesheet", "image", "media", "font", "script", "texttrack",
+    "xhr", "fetch", "eventsource", "websocket", "manifest", "other"
+]
+DEFAULT_RESOURCE_EXCLUSIONS = ["image", "stylesheet", "media", "font", "other"]
