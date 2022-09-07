90703538fb1072b67ed4ae9aacc2e0f82560348f
zigai <ziga.ivansek@gmail.com>
2022-09-07 18:13:33 +0200

    feat: convert obj to dict

--- wrighter/utils.py
+++ wrighter/utils.py
@@ -1,6 +1,10 @@
+import json
 from collections.abc import Mapping
+from dataclasses import asdict, is_dataclass
 from pathlib import Path
-from typing import Any
+from typing import Any, Callable, Iterable, Mapping
+
+from pydantic import BaseModel
 
 
 def load_pydatic_obj(val: str | Path | Any | Mapping | None, cls):
@@ -12,3 +16,14 @@ def load_pydatic_obj(val: str | Path | Any | Mapping | None, cls):
     elif isinstance(val, Mapping):
         return cls(**val)
     raise TypeError(type(val))
+
+
+def to_dict(obj: str | Mapping | Iterable | BaseModel) -> dict | list[dict]:
+    if isinstance(obj, dict): return obj
+    if is_dataclass(obj): return asdict(obj)
+    if isinstance(obj, BaseModel): return obj.dict()
+    if isinstance(obj, str): return json.loads(obj)
+    if isinstance(obj, Mapping): return dict(**obj)
+    if isinstance(obj, Iterable):
+        return [to_dict(i) for i in obj]  # type:ignore
+    raise TypeError(type(obj))
