5fccf6992aad7fc474266dbe9ef9782012e0ab29
zigai <ziga.ivansek@gmail.com>
2022-09-14 16:19:34 +0200

    fix: iterable to dict conversion

--- wrighter/utils.py
+++ wrighter/utils.py
@@ -6,6 +6,8 @@ from typing import Any, Callable, Iterable, Mapping
 
 from pydantic import BaseModel
 
+JSON_TYPES = [bool, float, int, str, None, dict, tuple, list]
+
 
 def load_pydatic_obj(val: str | Path | Any | Mapping | None, cls):
     if val is None:
@@ -31,5 +33,11 @@ def to_dict(obj: str | Mapping | Iterable | BaseModel) -> dict | list[dict]:
     if isinstance(obj, Mapping):
         return dict(**obj)
     if isinstance(obj, Iterable):
-        return [to_dict(i) for i in obj]  # type:ignore
+        json_arr = []
+        for i in obj:
+            if type(i) in JSON_TYPES:
+                json_arr.append(i)
+            else:
+                json_arr.append(to_dict(i))
+        return json_arr
     raise TypeError(type(obj))
