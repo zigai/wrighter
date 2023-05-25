7d3c2c72f0a92e308a87fb0b0db6192fcefc477f
zigai <ziga.ivansek@gmail.com>
2023-05-25 13:16:27 +0200

    use pyproject.toml

--- setup.py
+++ setup.py
@@ -1,29 +1,3 @@
-import pathlib
+from setuptools import setup
 
-from setuptools import find_packages, setup
-
-HERE = pathlib.Path(__file__).parent
-README = (HERE / "README.md").read_text()
-REQUIREMENTS = (HERE / "requirements.txt").read_text()
-
-setup(
-    name="wrighter",
-    version="0.1.2",
-    description="Web scraping/browser automation framework built on Playwright",
-    long_description=README,
-    long_description_content_type="text/markdown",
-    author="Žiga Ivanšek",
-    author_email="ziga.ivansek@gmail.com",
-    url="https://github.com/zigai/wrighter",
-    license="MIT",
-    classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3 :: Only",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
-    packages=find_packages(),
-    install_requires=REQUIREMENTS,
-)
+setup()
